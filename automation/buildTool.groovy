60e3f53ecbdcd8feaf998ce45a2cb8df990d81be
areshka515 <ariel.naidorf@develeap.com>
2021-11-22 20:49:18 +0200

    added artifactory

--- automation/buildTool.groovy
+++ automation/buildTool.groovy
@@ -1,5 +1,14 @@
 def preBuild() {
     sh("mkdir build")
+
+    def artifactory_name = "artifactory"
+    def artifactory_repo = "conan-local"
+    def repo_url = 'https://github.com/memsharded/example-boost-poco.git'
+    def repo_branch = 'master'
+
+    
+    def server = Artifactory.server artifactory_name
+    def client = Artifactory.newConanClient()
 }
 
 def Build() {
