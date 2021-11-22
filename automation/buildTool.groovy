ecafc28360c3e113adab9960f492989921b6e97e
areshka515 <ariel.naidorf@develeap.com>
2021-11-22 21:34:48 +0200

    small fix

--- automation/buildTool.groovy
+++ automation/buildTool.groovy
@@ -7,12 +7,17 @@ def Build() {
     dir("build") {
         def server = Artifactory.server "artifactory"
         def client = Artifactory.newConanClient()
+        def serverName = client.remote.add server: server, repo: "conan-local"
+
+        client.run(command: "create . 1.0@")
+
         /*sh("conan install ..")
         sh("cmake ..")
         sh("cmake --build .")
         sh("./myapp")*/
 
-        def b = client.run(command: "install ..")
+        String command = "upload \"*\" --all -r ${serverName} --confirm"
+        def b = client.run(command: command)
         server.publishBuildInfo b
     }
 }
