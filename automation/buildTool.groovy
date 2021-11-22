b3656bfd712fd52b8981ff90fe2278b9d0b7c70e
areshka515 <ariel.naidorf@develeap.com>
2021-11-22 21:22:40 +0200

    small fix

--- automation/buildTool.groovy
+++ automation/buildTool.groovy
@@ -1,18 +1,17 @@
-
-def artifactory_name = "artifactory"
-def artifactory_repo = "conan-local"
-def serverName = client.remote.add server: server, repo: artifactory_repo
-def recipe_folder = "recipes/zlib/1.2.11"
-def recipe_version = "1.2.11"
-
-def server = Artifactory.server artifactory_name
-def client = Artifactory.newConanClient()
-
 def preBuild() {
     sh("mkdir build")
 }
 
 def Build() {
+    def artifactory_name = "artifactory"
+    def artifactory_repo = "conan-local"
+    def serverName = client.remote.add server: server, repo: artifactory_repo
+    def recipe_folder = "recipes/zlib/1.2.11"
+    def recipe_version = "1.2.11"
+
+    def server = Artifactory.server artifactory_name
+    def client = Artifactory.newConanClient()
+    
     dir("build") {
         /*sh("conan install ..")
         sh("cmake ..")
