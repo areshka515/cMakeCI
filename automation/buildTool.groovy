94b4a6b2787c78771936f8706bd3337603f68720
areshka515 <ariel.naidorf@develeap.com>
2021-11-24 16:17:05 +0200

    branch

--- automation/buildTool.groovy
+++ automation/buildTool.groovy
@@ -1,14 +1,13 @@
 def conanServer
 def conanClient
 def conanServerName
-def VERSION
+def VERSION // version of the artifcat
 def CHANNEL // [release, nightly, feature]
 def CLIENT // [nightly, develeap]
 
 def createVersion() {
     prefix = "origin/"
     branchname = env.GIT_BRANCH.substring(prefix.size())
-    CHANNEL = branchname == "master" ? "stable" : "release"
     //ver = sh(script: "echo ${branchname} | cut -d _ -f 2", returnStdout: true).trim()
     echo "${branchname}"
     //|| branchname.contains("feature/")
@@ -26,10 +25,25 @@ def createVersion() {
         }
         VERSION = newtag
         CLIENT = "rgo"
+        CHANNEL = "stable"
     }
     else if(branchname.startsWith("release")) {
         releasebranch = branchname.split('\\_')
-        echo "${releasebranch}"
+        lasttag = sh(script: "git tag -l --sort=version:refname \"${releasebranch[3]}.*\" | tail -1", returnStdout: true).trim()
+        def newtag
+        if (lasttag.isEmpty()) {
+            sh "git tag ${releasebranch[3]}.0"
+            newtag = "${releasebranch[3]}.0"
+        } else {
+            newtag = lasttag.split('\\.')
+            newtag[1] = newtag[1].toInteger() + 1
+            newtag = newtag.join('.')
+            sh "git tag ${newtag}"
+        }
+        VERSION = newtag
+        CLIENT = releasebranch[1]
+        CHANNEL = "release"
+        echo "${VERSION}"
     }
 }
 
