ca95b59bd6843e856a7dffb47d8d0e380c326cf7
areshka515 <ariel.naidorf@develeap.com>
2021-11-24 16:02:46 +0200

    branch

--- automation/buildTool.groovy
+++ automation/buildTool.groovy
@@ -9,7 +9,7 @@ def createVersion() {
     prefix = "origin/"
     branchname = env.GIT_BRANCH.substring(prefix.size())
     CHANNEL = branchname == "master" ? "stable" : "release"
-    ver = sh(script: "echo ${branchname} | cut -d / -f 2", returnStdout: true).trim()
+    ver = sh(script: "echo ${branchname} | cut -d _ -f 2", returnStdout: true).trim()
     echo "${branchname}"
     echo "${ver}"
     //|| branchname.contains("feature/")
